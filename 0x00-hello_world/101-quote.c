0d57bdf793aedf5b5ac6bb93c4c3adb8a1b91083
ShehabNegm <shebonegm@gmail.com>
2023-02-16 16:17:51 -0800

    modify the file to betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+  *main - function main
+  *Description: function will print a quote into command line from c file
+  *Return: function will return 1
+  */
 int main(void)
 {
-	system ("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19' >&2");
+	system("echo 'and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19' >&2");
 	return (1);
 }
