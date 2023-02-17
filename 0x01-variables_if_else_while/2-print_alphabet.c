1ea747d770054b4c6f1dc3cd9d2d5ef0ab29a67a
ShehabNegm <shebonegm@gmail.com>
2023-02-17 11:20:08 -0800

    using putchar to print alphapet letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - function main
+  * Description: function will print all alphapet using putchar()
+  * Return: function will return all alphapet in lower case
+  */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ++ch)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
