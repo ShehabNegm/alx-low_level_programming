825aac35747a65e317cf1d3fb224473c2f94fdd3
ShehabNegm <shebonegm@gmail.com>
2023-02-17 14:32:35 -0800

    adding correct files for the checker

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+  * main - function main
+  * Description: print all numbers less than 10 using putchar
+  * Return: function will return numbers less than 10 from 0
+  */
+int main(void)
+{
+	int i = 0;
+	int b;
+
+	while (i < 10)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (i < b)
+			{
+				putchar((i) + '0');
+				putchar((b) + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else
+				{
+					putchar(' ');
+				}
+			}
+		}
+		++i;
+	}
+	return (0);
+}
