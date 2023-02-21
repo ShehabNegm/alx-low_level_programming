2fbc566bb58c3f6a71348983d4a8d830d962da10
ShehabNegm <shebonegm@gmail.com>
2023-02-21 13:48:33 -0800

    function that uses another function to return alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - will return all alphabet
+  * Description: function that print a alphabet 10 times
+  * Return: return alway zero
+  */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		print_alphabet();
+		i++;
+	}
+}
