948e825820a027a3ea72ce90fa165e2b9e31aa57
ShehabNegm <shebonegm@gmail.com>
2023-02-22 11:37:30 -0800

    fix the function for debugging

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * largest_number - returns the largest number of 3 ints
+  * @a: first int
+  * @b: second int
+  * @c: third int
+  * Return: largest number
+  */
+
+int largest_number(int a, int b, int c)
+{
+	int lrg;
+
+	if (a > b && a > c)
+	{
+		lrg = a;
+	}
+	else if (b > a && b > c)
+	{
+		lrg = b;
+	}
+	else
+	{
+		lrg = c;
+	}
+	return (lrg);
+}
