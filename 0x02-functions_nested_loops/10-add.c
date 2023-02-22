67d9e2056289c17a0fa0b4a2f1126e3726a0b918
ShehabNegm <shebonegm@gmail.com>
2023-02-21 17:48:30 -0800

    command that add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * add - will return table addition of 2 numbers
+  *@a : first int
+  *@b : second int
+  * Description: function will return addition
+  * Return: addition
+  */
+int add(int a, int b)
+{
+	int ad;
+
+	ad = a + b;
+		return (ad);
+}
