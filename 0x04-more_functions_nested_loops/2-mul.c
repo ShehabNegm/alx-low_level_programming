46e9321807dc662667a9c37af20ee5ec114b9e26
ShehabNegm <shebonegm@gmail.com>
2023-02-23 12:16:57 -0800

    function that will multiply two ints

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * mul - return multiply of two ints
+  * @i : input as int
+  * @s : input as int
+  *
+  * Return: function will return multiply of two ints
+  */
+
+int mul(int i, int s)
+{
+	return (i * s);
+}
