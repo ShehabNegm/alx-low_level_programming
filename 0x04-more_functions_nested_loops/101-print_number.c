81f3c9811e3bf9d81645af297f7fcc0e31c24c58
ShehabNegm <shebonegm@gmail.com>
2023-02-24 12:54:59 -0800

    change the code logic to solve using recurrsion

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,42 +8,14 @@
 
 void print_number(int i)
 {
-	if (i > 0 && i < 10)
-		_putchar((i % 10) + '0');
-	else if (i == 0)
-		_putchar(48);
-	else if (-i > 10 && -i < 100)
+	unsigned int num = i;
+
+	if (i < 0)
 	{
 		_putchar('-');
-		if (-i < 100)
-		{
-			_putchar((-i / 10) + '0');
-		}
-		_putchar((-i % 10) + '0');
+		num = -num;
 	}
-	else if (-i > 100 && -i <= 999)
-	{
-		_putchar('-');
-		_putchar((-i / 100) + '0');
-		_putchar((-i / 1000) + '0');
-		_putchar((i % 100) + '0');
-	}
-	else if (i >= 10 && i < 100)
-	{
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-	}
-	else if (i >= 100 && i <= 999)
-	{
-		_putchar((i / 100) + '0');
-		_putchar((i / 1000) + '0');
-		_putchar((i % 100) + '0');
-	}
-	else if (i >= 1000 && i <= 9999)
-	{
-		_putchar((i / 1000) + '0');
-		_putchar((i / 1100) + '0');
-		_putchar((i / 10 % 10) + '0');
-		_putchar((i % 10) + '0');
-	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + 48);
 }
