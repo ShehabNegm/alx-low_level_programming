6b39a4e3dbb23712f3a1eb9bce9aa3f46dfa5554
ShehabNegm <shebonegm@gmail.com>
2023-02-23 17:17:14 -0800

    function that find largest factor number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - check the largest factor number
+ * Return: largest factor number of int
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int i = 2; /* intial value */
+	long int fct; /*factor*/
+	long int lfct = 0; /*largest factor*/
+
+	while (i < n)
+	{
+		while (n % i == 0)
+		{
+			fct = i;
+			n = n / fct;
+			if (fct > lfct)
+			{
+				lfct = fct;
+			}
+		}
+		i++;
+	}
+	return (lfct);
+}
+
