b77413bc522e562c9f34fd965abfb19078896949
ShehabNegm <shebonegm@gmail.com>
2023-02-27 10:55:07 -0800

    solve first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - will reset a value a pointers points to 98
+  * @i: input pointer
+  *
+  * Return: will return value of pointer to 98
+  */
+
+void reset_to_98(int *i)
+{
+	*i = 98;
+}
