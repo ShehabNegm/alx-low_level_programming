3347b9f3bae3f6b24917134b6d04f7ee336fd44a
ShehabNegm <shebonegm@gmail.com>
2023-02-27 15:34:06 -0800

    function that will print the n items of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_array - will print first n items of an array
+  * @n: input int
+  * @arr: input array
+  * Return: will print first n items of array
+  */
+
+void print_array(int *arr, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d\n", *(arr + i));
+		else
+			printf("%d, ", *(arr + i));
+	}
+}
