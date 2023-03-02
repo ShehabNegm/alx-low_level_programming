0eb786387624bd51902540aa6e754f6862e5cd17
ShehabNegm <shebonegm@gmail.com>
2023-03-02 15:36:06 -0800

    fix function

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,15 +15,16 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; ++i)
 	{
-		if ((str[i] > 0 && str[i] < 48) || str[i] == 59)
+		if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			if (str[i] == 32 || str[i] == 9 || str[i] == 10 ||
+				str[i] == 44 || str[i] == 59 || str[i] == 46 ||
+				str[i] == 33 || str[i] == 63 || str[i] == 34 ||
+				str[i] == 40 || str[i] == 41 || str[i] == 123 ||
+				str[i] == 125)
+			{
 				str[i + 1] = str[i + 1] - 32;
-		}
-		else if (str[i] == 63 || str[i] == 123 || str[i] == 125)
-		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i + 1] = str[i + 1] - 32;
+			}
 		}
 	}
 	return (str);
