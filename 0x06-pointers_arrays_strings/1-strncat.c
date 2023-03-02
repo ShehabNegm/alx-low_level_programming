6b0a78e981a35f1e135bd19644db2128471a7aac
ShehabNegm <shebonegm@gmail.com>
2023-03-02 12:22:28 -0800

    fix last checker

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (len = 0; dest[len] != '\0'; ++len)
 		;
-	for (i = 0; i < n; ++i)
+	for (i = 0; i < n && src[i] != '\0'; ++i)
 	{
 		dest[len] = src[i];
 		len++;
