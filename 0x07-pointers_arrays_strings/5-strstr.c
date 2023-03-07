b5c63db984ae0a0cf501222b8920dd2dc234bd2e
ShehabNegm <shebonegm@gmail.com>
2023-03-07 00:36:11 -0800

    fix last checker

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,24 +14,19 @@ char *_strstr(char *haystack, char *needle)
 	int j = 0;
 	int f;
 
-	if (needle[0] == '\0' && haystack[0] == '\0')
-		return (&haystack[0]);
 
 	while (haystack[j] != '\0')
 	{
 		i = 0;
-		if (needle[0] == haystack[j])
+		f = j;
+		while (needle[i] == haystack[j] && needle[i] != '\0')
 		{
-			f = j;
-			while (needle[i] == haystack[j] && needle[i] != '\0')
-			{
-				i++;
-				j++;
-			}
-			if (needle[i] == '\0')
-				return (&haystack[f]);
+			i++;
+			j++;
 		}
-		j++;
+		if (needle[i] == '\0')
+			return (&haystack[f]);
+	j++;
 	}
 	return ('\0');
 }
