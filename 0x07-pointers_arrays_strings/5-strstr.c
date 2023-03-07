2db3c2b16809577ef7185b328a3bcdacde5ff935
ShehabNegm <shebonegm@gmail.com>
2023-03-06 17:16:09 -0800

    function to find sub string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * _strstr - will locate substring in a string
+  * @haystack : input string to be searched
+  * @needle : input substring to be searched for
+  *
+  * Return: will return pointer to first occurence of substring
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j = 0;
+	int f;
+
+	while (haystack[j] != '\0')
+	{
+		i = 0;
+		if (needle[0] == haystack[j])
+		{
+			f = j;
+			while (needle[i] == haystack[j] && needle[i] != '\0')
+			{
+				i++;
+				j++;
+			}
+			if (needle[i] == '\0')
+				return (&haystack[f]);
+		}
+		j++;
+	}
+	return ('\0');
+}
