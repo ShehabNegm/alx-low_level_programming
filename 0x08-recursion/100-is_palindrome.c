38a28aea51bff5f867392f28771165ac48b40725
ShehabNegm <shebonegm@gmail.com>
2023-03-09 14:45:41 -0800

    function to check palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * is_palindrome - will check if int is prime
+  * @s : input char
+  *
+  * Return: return int
+  */
+
+int is_palindrome(char *s)
+{
+	if (s[0] == s[1])
+		return (1);
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,4 @@ int _sqrt_recursion(int n);
 int _sqrt(int n, int x);
 int is_prime_number(int n);
 int _prime(int n, int x);
+int is_palindrome(char *s);
