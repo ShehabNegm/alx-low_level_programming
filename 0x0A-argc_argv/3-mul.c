42f19270cab69fed238d1ab66f3b95e0cbb4b74a
ShehabNegm <shebonegm@gmail.com>
2023-03-10 14:30:00 -0800

    function to multiply argv arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - main function
+  * @argc : input int
+  * @argv : input pointers to array of strings
+  *
+  * Return: will always return 0 sucess
+  */
+
+int main(int argc, char *argv[])
+{
+	int multi;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		multi = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", multi);
+	}
+	return (0);
+}
