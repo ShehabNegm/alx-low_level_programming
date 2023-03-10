74c8a85f345b0a6a102929aea69cd5ef24c6eb25
ShehabNegm <shebonegm@gmail.com>
2023-03-10 13:56:47 -0800

    function to return number of arguments argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - main function
+  * @argc : input int
+  * @argv : input pointers to array of strings
+  *
+  * Return: will always return 0 sucess
+  */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
