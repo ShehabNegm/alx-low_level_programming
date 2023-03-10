9b822d20663d95b188daf9ce1b6b8041fc56f75e
ShehabNegm <shebonegm@gmail.com>
2023-03-10 14:58:08 -0800

    function to add argv arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - main function
+  * @argc : input int
+  * @argv : input pointers to array of strings
+  *
+  * Return: will always return 0 sucess
+  */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
