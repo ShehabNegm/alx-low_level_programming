d77e2f43b3400054766ed843943002f4fa7aec64
ShehabNegm <shebonegm@gmail.com>
2023-03-10 16:29:50 -0800

    fix last checker

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,22 +14,17 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int i;
 
-	if (argc < 2)
-		printf("0\n");
-	else
+	for (i = 1; i < argc; ++i)
 	{
-		for (i = 1; i < argc; ++i)
+		if (atoi(argv[i]) == 0 && argv[i][i] != 0)
 		{
-			if (atoi(argv[i]) == 0)
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("Error\n");
+			return (1);
+		}
 
-			else
-			{
-			sum += atoi(argv[i]);
-			}
+		else
+		{
+		sum += atoi(argv[i]);
 		}
 	}
 	printf("%d\n", sum);
