b202c82575630f3a1ad4762a3e452c5e09bad3f6
ShehabNegm <shebonegm@gmail.com>
2023-03-10 16:44:45 -0800

    last task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,12 +14,12 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int i;
 
-	if (argc < 2)
-		printf("0\n");
+	if (argc < 1)
+		return (0);
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]) == 0)
+		if (!atoi(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
