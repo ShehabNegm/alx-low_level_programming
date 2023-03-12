c3becf021cecec16c90c46156e223cccc22b5068
ShehabNegm <shebonegm@gmail.com>
2023-03-12 13:28:17 -0700

    advanced task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - main function
+  * @argc : input int
+  * @argv : input pointers to array of strings
+  * Return: will always return 0 sucess
+  */
+int main(int argc, char *argv[])
+{
+	int chng = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		int p = atoi(argv[1]);
+
+		while (p > 1)
+		{
+			if (p >= 25)
+			{
+				chng += p / 25;
+				p = p % 25;
+			}
+			else if (p >= 10 && p < 25)
+			{
+				chng += p / 10;
+				p = p % 10;
+			}
+			else if (p >= 5 && p < 10)
+			{
+				chng += p / 5;
+				p = p % 5;
+			}
+			else if (p >= 2 && p < 5)
+			{
+				chng += p / 2;
+				p = p % 2;
+			}
+		}
+		if (p == 1)
+		{
+			chng += 1;
+		}
+	}
+	printf("%d\n", chng);
+	return (0);
+}
