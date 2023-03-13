b1104df30a4a37e8f6be84886d55ba91189d4672
ShehabNegm <shebonegm@gmail.com>
2023-03-13 12:36:44 -0700

    function to allocate dynamic memory and fill it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * create_array - will create array using dynamic memory location
+  * @c : input char
+  * @size : size of char array
+  * Return: will return pointer to the array of char type
+  */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *p = (char *) malloc(size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			p[i] = c;
+		}
+	}
+	return (p);
+}
