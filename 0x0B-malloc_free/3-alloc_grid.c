2bb654eb3e5da302b681e7f6fbd2b47dd8cdb7d3
ShehabNegm <shebonegm@gmail.com>
2023-03-13 17:13:56 -0700

    dynamic memorry allocation for 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * alloc_grid - 2d array of integers
+  * @width : input int
+  * @height : input int
+  * Return: will return pointer 2d array of integers
+  */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int j;
+	int **p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+
+	p = (int **) malloc(sizeof(int) * width);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			p[i] = malloc(sizeof(int) *width);
+			if (p[i] == NULL)
+			{
+				return(NULL);
+			}
+			else
+			{
+				for (j = 0; j < width; j++)
+				{
+					p[i][j] = 0;
+				}
+			}
+		}
+	}
+	return (p);
+}
