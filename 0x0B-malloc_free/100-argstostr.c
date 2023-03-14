ebca25a5142ae8c915f2d063765acbca610f4a0d
ShehabNegm <shebonegm@gmail.com>
2023-03-14 14:45:34 -0700

    last task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * argstostr - free memorry of 2d array
+  * @ac : input int number of arguments
+  * @av : input array of strings
+  * Return: will return pointer to new string
+  */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int charc = 0; /*count char inside argv*/
+	char *p;
+	int j;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			charc++;
+		}
+
+	}
+	p = malloc(charc + ac);
+	
+	if (p == NULL)
+	{
+		free (p);
+		return (NULL);
+	}
+
+	for (j = 0; j < ac; j++)
+	{
+		for (i = 0; av[j][i] != '\0'; i++)
+		{
+			*p = av[j][i];
+			p++;
+		}
+		*p = '\0';
+		p++;
+		*p = '\n';
+		p++;
+	}
+	return (p);
+}
+
+/**
+  * main - main function
+  * @argc : input int number of arguments
+  * @argv : input array of string
+  * Return: will return 0 or 1
+  */
+  
+
+int main(int argc, char *argv[])
+{
+
+	argstostr(argc, argv);
+	return (0);
+}
