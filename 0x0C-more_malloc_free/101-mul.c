9365f744fd326352af7eb0b26727a11ae266cb04
ShehabNegm <shebonegm@gmail.com>
2023-03-17 03:07:29 -0700

    solve last task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - main function
+  * @argc : input int
+  * @argv : input pointers to array of strings
+  *
+  * Return: will always return 0 sucess
+  */
+
+int main(int argc, char *argv[])
+{
+	unsigned long int multi;
+	int i, j;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	multi = atoi(argv[1]) * atoi(argv[2]);
+	printf("%lu\n", multi);
+	return (0);
+}
