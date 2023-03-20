1b5384a2d76e3c03c96439252dbf992545ba3e26
ShehabNegm <shebonegm@gmail.com>
2023-03-20 16:26:56 -0700

    fix last checker

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,26 +21,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
-	{
-		free(dog);
 		return (NULL);
-	}
+
 	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (dog->name == NULL)
 	{
-		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
 
 	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (dog->owner == NULL)
 	{
-		free(dog->owner);
+		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
 
-	strcpy(dog->name, name);
-	strcpy(dog->owner, owner);
+	dog->name = strcpy(dog->name, name);
+	dog->owner = strcpy(dog->owner, owner);
 	dog->age = age;
 	return (dog);
 }
