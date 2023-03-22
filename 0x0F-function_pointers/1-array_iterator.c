0a237d7628ece44e8e56c3639e410a9efe40aff8
ShehabNegm <shebonegm@gmail.com>
2023-03-22 13:48:27 -0700

    fix pointers handling

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -11,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (!action)
+	if (!action || !array)
 		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
