aabcfd52fc7ac0bfa8d10312cabe69fd41e202b4
ShehabNegm <shebonegm@gmail.com>
2023-03-31 07:54:55 -0700

    add function to print before main function execution

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+  * begin - function to print before main execution
+  */
+void begin(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
