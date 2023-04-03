e131b13dc637a8be6ea71b6985041c4ab98f4dc4
ShehabNegm <shebonegm@gmail.com>
2023-04-03 13:24:32 -0700

    fix checker for free head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,15 @@ void free_listint2(listint_t **head)
 	listint_t *tmp;
 	listint_t *tmph = *head;
 
-	while (tmph != NULL && *head != NULL)
+	if (head != NULL)
 	{
-		tmp = tmph;
-		tmph = tmph->next;
-		free(tmp);
+		while (tmph != NULL && *head != NULL)
+		{
+			tmp = tmph;
+			tmph = tmph->next;
+			free(tmp);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
 
