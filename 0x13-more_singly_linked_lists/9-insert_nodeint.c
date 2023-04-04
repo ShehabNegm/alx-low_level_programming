ccfa4c1efcdfff670f645693c0d8c043ab848789
ShehabNegm <shebonegm@gmail.com>
2023-04-04 13:46:42 -0700

    fix last checker

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		len++;
 		tmp = tmp->next;
 	}
-	if (idx > len)
+	if (idx > len && (idx - len) > 1)
 	{
 		free(new);
 		return (NULL);
