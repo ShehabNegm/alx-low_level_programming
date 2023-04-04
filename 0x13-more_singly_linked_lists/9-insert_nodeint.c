2bcd7939c61e5be7827a83ba574ed8d416ad5356
ShehabNegm <shebonegm@gmail.com>
2023-04-04 13:24:43 -0700

    fix error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	new->n = n;
 
-	for (i = 0; i < (idx - 1); i++)
+	for (i = 0; i < (idx - 1) && tmp != NULL; i++)
 	{
 		tmp = tmp->next;
 	}
