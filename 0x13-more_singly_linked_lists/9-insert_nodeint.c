513d90c97ebae50c10aba031b5cc229bd4edd55e
ShehabNegm <shebonegm@gmail.com>
2023-04-04 13:36:35 -0700

    fix zero index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,11 +32,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	new->n = n;
 
-	for (i = 0; i < (idx - 1) && tmp != NULL; i++)
+	if (idx == 0)
 	{
-		tmp = tmp->next;
+		new->next = *head;
+		*head = new;
 	}
-	new->next = tmp->next;
-	tmp->next = new;
+	else
+	{
+		for (i = 0; i < (idx - 1) && tmp != NULL; i++)
+			tmp = tmp->next;
+		new->next = tmp->next;
+		tmp->next = new;
+	}
 	return (new);
 }
