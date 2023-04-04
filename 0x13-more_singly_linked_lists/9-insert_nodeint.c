19a37de42dd713c77552ee4dc5661892bf439002
ShehabNegm <shebonegm@gmail.com>
2023-04-04 13:07:59 -0700

    fix index injecting

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,13 +24,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		len++;
 		tmp = tmp->next;
 	}
-	if (idx > len)
+	if (idx >= len)
 		return (NULL);
 
 	tmp = *head;
 	new->n = n;
 
-	for (i = 0; i < idx && tmp != NULL; i++)
+	for (i = 0; i < (idx - 1) && tmp != NULL; i++)
 	{
 		tmp = tmp->next;
 	}
