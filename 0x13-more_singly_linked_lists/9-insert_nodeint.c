5e5c9b4a7d8fb3c8c6da032a8b1f0922a4ade285
ShehabNegm <shebonegm@gmail.com>
2023-04-04 13:20:28 -0700

    insert node at index of a linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,8 +25,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp = tmp->next;
 	}
 	if (idx > len)
+	{
+		free(new);
 		return (NULL);
-
+	}
 	tmp = *head;
 	new->n = n;
 
