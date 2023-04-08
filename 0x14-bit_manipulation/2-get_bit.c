f233cca3806b4ed730cea3acc310edd92df57aa0
ShehabNegm <shebonegm@gmail.com>
2023-04-08 07:52:12 -0700

    function to get value of bit at  agiven index using bits manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * get_bit - will returns value of bin at a given index
+  * @n : input int
+  * @index : input index
+  * Return: will return 0 or 1
+  */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+	if (index > 32)
+		return (0);
+	if ((n & (1 << index)) != 0)
+		return (1);
+	else
+		return (0);
+}
+
