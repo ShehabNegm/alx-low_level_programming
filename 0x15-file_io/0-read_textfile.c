c98b2a2e47073b840b9f445dc77be1e9a6c6616e
ShehabNegm <shebonegm@gmail.com>
2023-04-09 15:00:18 -0700

    try to fix errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,20 +10,33 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t sz, wr;
-	char *c = calloc(letters, sizeof(char));
+	char *c = malloc(letters * sizeof(char));
 
-	if (!*filename)
+	if (filename == NULL || letters == 0)
+	{
+		free(c);
 		return (0);
+	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
+	{
+		free(c);
 		return (0);
+	}
 
 	sz = read(fd, c, letters);
 	if (sz < 0)
+	{
+		free(c);
 		return (0);
+	}
+
 	wr = write(STDOUT_FILENO, c, sz);
 	if (wr < 0 || wr != sz)
+	{
+		free(c);
 		return (0);
+	}
 	return (wr);
 }
