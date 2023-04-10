005cc6d1549ed8efc21da629563b64e0725f1543
ShehabNegm <shebonegm@gmail.com>
2023-04-10 13:28:12 -0700

    try to fix file writing permission

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,14 +9,16 @@
 
 int main(int argc, char *argv[])
 {
-	int cr, fd, fd2, rd, c1, c2, wr;
-	char *buffer = malloc(1024 * sizeof(char));
+	int cr, fd, rd, c1, c2, wr;
+	char *buffer;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	buffer = malloc(1024 * sizeof(char));
+
 	fd = open(argv[1], O_RDONLY);
 	rd = read(fd, buffer, 1024);
 	if (fd < 0 || argv[1] == NULL || rd == -1)
@@ -25,20 +27,19 @@ int main(int argc, char *argv[])
 		free(buffer);
 		exit(98);
 	}
-	cr = creat(argv[2], 0664);
-	fd2 = open(argv[2], O_CREAT || O_WRONLY || O_TRUNC);
-	wr = write(fd2, buffer, rd);
-	if (cr < 0 || wr < 0 || buffer == NULL || fd2 < 0)
+	cr = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	wr = write(cr, buffer, rd);
+	if (cr < 0 || wr < 0 || buffer == NULL || cr < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		free(buffer);
 		exit(99);
 	}
 	c1 = close(fd);
-	c2 = close(fd2);
+	c2 = close(cr);
 	if (c1 < 0 || c2 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c1 > c2 ? fd2 : fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c1 > c2 ? cr : fd);
 		exit(100);
 	}
 	free(buffer);
