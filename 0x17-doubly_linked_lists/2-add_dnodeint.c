ab48a538c656760006ebb0b52b32f0069490b9e8
ShehabNegm <shebonegm@gmail.com>
2023-05-18 14:22:55 -0700

    function to add a new node as head of a d linked list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+  * add_dnodeint - function to insert element at the head of d linked list
+  * @head: header of the doubly linked list
+  * @n: element of the new node
+  * Return: will return the new head or NULL
+  */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+
+	if (*head != NULL)
+		(*head)->prev = new;
+
+	return (new);
+}
