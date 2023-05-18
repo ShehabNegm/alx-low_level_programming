837c61527b178564af10e21ee77ce579d2aadc5a
ShehabNegm <shebonegm@gmail.com>
2023-05-18 15:25:38 -0700

    function to get node at indx of a double linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+  * dlistint_len - function to return length of d linked list
+  * @h: header of the doubly linked list
+  * Return: will return length of the list
+  */
+
+size_t len(dlistint_t *h)
+{
+	size_t length = 0;
+	dlistint_t *tmp = h;
+
+	while (tmp != NULL)
+	{
+		length++;
+		tmp = tmp->next;
+	}
+	return (length);
+}
+
+/**
+  * get_dnodeint_at_index - function to get node at index
+  * @head: header of the doubly linked list
+  * @index: index of the required node
+  * Return: will return the node at index or NULL
+  */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t length = len(head);
+	unsigned int i;
+	dlistint_t *tmp;
+
+	if (index >= length || !head)
+		return (NULL);
+
+	tmp = head;
+
+	for (i = 0; i < index && tmp != NULL; i++)
+	{
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
