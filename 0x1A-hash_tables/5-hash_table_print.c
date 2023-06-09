3085d7fdb459fb7e8c198a04f8bc03d854c33932
ShehabNegm <shebonegm@gmail.com>
2023-06-09 15:04:12 -0700

    fix betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,34 +8,41 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int size = ht->size, i, j;
-	printf("{");
 
-	for (i = 0; i < size; i++)
+	if (ht != NULL)
 	{
-		if (ht->array[i])
+
+		printf("{");
+
+		for (i = 0; i < size; i++)
 		{
-			hash_node_t *tmp = ht->array[i];
-			printf("'%s': '%s'", tmp->key, tmp->value);
-			while (tmp->next != NULL)
+			if (ht->array[i])
 			{
-				printf(", '%s': '%s'", tmp->key, tmp->value);
-				tmp = tmp->next;
-			}
-			j = i + 1;
-			break;
-		}
-	}
-	for (j = j; j < size; j++)
-	{
-		if (ht->array[j])
-		{
-			hash_node_t *tmp = ht->array[j];
-			while (tmp != NULL)
-			{
-				printf(", '%s': '%s'", tmp->key, tmp->value);
-				tmp = tmp->next;
+				hash_node_t *tmp = ht->array[i];
+
+				printf("'%s': '%s'", tmp->key, tmp->value);
+				while (tmp->next != NULL)
+				{
+					printf(", '%s': '%s'", tmp->key, tmp->value);
+					tmp = tmp->next;
+				}
+				j = i + 1;
+				break;
 			}
 		}
+		for (j = j; j < size; j++)
+		{
+			if (ht->array[j])
+			{
+				hash_node_t *tmp = ht->array[j];
+
+				while (tmp != NULL)
+				{
+					printf(", '%s': '%s'", tmp->key, tmp->value);
+					tmp = tmp->next;
+				}
+			}
+		}
+		printf("}\n");
 	}
-	printf("}\n");
 }
