a51f4ab505b1e74ec51717dfc3d6b8a4c41f1e9f
ShehabNegm <shebonegm@gmail.com>
2023-06-09 16:16:11 -0700

    fix last checker

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -16,6 +16,8 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		head = ht->array[i];
+		if (head == NULL)
+			free(ht->array[i]);
 		while ((tmp = head) != NULL)
 		{
 			head = head->next;
@@ -23,6 +25,7 @@ void hash_table_delete(hash_table_t *ht)
 			free(tmp->key);
 			free(tmp);
 		}
+		free(head);
 	}
 	free(ht->array);
 	free(ht);
