0e2ffde2ae9a44c0069ab0b98a5ee77b308b476e
ShehabNegm <shebonegm@gmail.com>
2023-06-09 14:58:21 -0700

    add function to ptint hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_print - function to print a hash table
+  * @ht: hash table to be printed
+  */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int size = ht->size, i, j;
+	printf("{");
+
+	for (i = 0; i < size; i++)
+	{
+		if (ht->array[i])
+		{
+			hash_node_t *tmp = ht->array[i];
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			while (tmp->next != NULL)
+			{
+				printf(", '%s': '%s'", tmp->key, tmp->value);
+				tmp = tmp->next;
+			}
+			j = i + 1;
+			break;
+		}
+	}
+	for (j = j; j < size; j++)
+	{
+		if (ht->array[j])
+		{
+			hash_node_t *tmp = ht->array[j];
+			while (tmp != NULL)
+			{
+				printf(", '%s': '%s'", tmp->key, tmp->value);
+				tmp = tmp->next;
+			}
+		}
+	}
+	printf("}\n");
+}
