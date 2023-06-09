e2c327c8cdb84c514b8e59496812597eb34db5b8
ShehabNegm <shebonegm@gmail.com>
2023-06-09 15:57:52 -0700

    add function to free hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_delete - function to delete a hash table
+  * @ht: hash table to be deleted
+  */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp, *head;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		head = ht->array[i];
+		if (head == NULL)
+			free(ht->array[i]);
+		while (head != NULL)
+		{
+			tmp = head;
+			head = head->next;
+			free(tmp->value);
+			free(tmp->key);
+			free(tmp);
+		}
+		free(head);
+	}
+	free(ht->array);
+	free(ht);
+}
