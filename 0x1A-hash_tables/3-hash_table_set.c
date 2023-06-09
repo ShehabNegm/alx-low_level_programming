97bda1a088f53881f4d97d690a724d2e45901d19
ShehabNegm <shebonegm@gmail.com>
2023-06-09 12:46:32 -0700

    function to set a hash table by adding key value pairs, also manage collision

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_set - function to add element to the hash table
+  * @ht: hash table to add elements to
+  * @key: element key
+  * @value: element value
+  * Return: 1 if succeeded 0 otherwise
+  */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = node;
+		node->next = NULL;
+		return (1);
+	}
+	else
+	{
+		hash_node_t *head = ht->array[index];
+
+		node->next = head;
+		ht->array[index] = node;
+	}
+	return (1);
+}
